2a699090a8b13a93d1c4e5cbc8994211c454b37e
jweboy <1259524459@qq.com>
2020-01-07 22:43:19 +0800

    :sparkles: Add miniCSS,fileSize, formatErrMsg

--- scripts/webpack/getWebpackConfig/dev.js
+++ scripts/webpack/getWebpackConfig/dev.js
@@ -1,3 +1,11 @@
-const webpackConfig = require('./index');
+/*
+ * @Author: jweboy
+ * @Date: 2020-01-06 10:36:54
+ * @LastEditors: jweboy
+ * @LastEditTime: 2020-01-06 18:22:55
+ */
+const getWebpackConfig = require('./index');
 
-module.exports = webpackConfig;
+const webpackConfig = getWebpackConfig();
+
+module.exports = webpackConfig.toConfig();
